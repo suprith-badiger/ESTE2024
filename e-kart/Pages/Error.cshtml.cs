0115903fee01fa607233747fded171bfc269c354
suprith-badiger <suprithbadiger02@gmail.com>
2024-11-04 19:46:33 +0530

    initial config

--- e-kart/Pages/Error.cshtml.cs
+++ e-kart/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EKart.Pages;
+namespace e_kart.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
--- e-kart/Pages/Privacy.cshtml.cs
+++ e-kart/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EKart.Pages;
+namespace e_kart.Pages;
 
 public class PrivacyModel : PageModel
 {
